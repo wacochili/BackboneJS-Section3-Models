e07d5aaee019cb2ab01e291dd3e70c09e1afb3f4
Rich Baukovic <rich.baukovic@brickfish.com>
2017-01-30 21:59:24 -0600

    WIP section 3 Models project initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// In the first few sections, we do all the coding here.
+// Later, you'll see how to organize your code into separate
+// files and modules.
+
+var Vehicle = Backbone.Model.extend({
+  idAttribute: "registrationNumber",
+  urlRoot: "api/vehicles",
+
+
+  validate: function(attrs){
+    if (!attrs.registrationNumber)
+      return "Registration Number is required.";
+  }
+  
+  start: function(){
+    console.log("Vehicle started")
+  }
+});
+
+var Car = Vehicle.extend({
+  start: function(){
+    console.log("Car with registration number {registrationNumber} started.")
+  }
+})
